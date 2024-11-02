95ad25f99423cd812755d69a7e7406238ff54537
hybanth <hybanth@gmail.com>
2024-11-02 23:41:59 +0530

    restructuring to seperate expressApp and webServer

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
-
-const Note = require('./models/notes')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
